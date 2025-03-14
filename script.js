3f40f35677e43d52abfbebe8b86c304b815f18ea
pankaj-comfygen <pankaj.comfy@gmail.com>
2025-03-14 20:54:16 +0530

    save

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ document.getElementById("header").innerHTML = fetch('header.html')
 document.getElementById("footer").innerHTML = fetch('footer.html')
     .then(response => response.text())
     .then(data => document.getElementById("footer").innerHTML = data);
+
+
+
+
+
